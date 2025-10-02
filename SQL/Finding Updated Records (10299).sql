ac094b2a0a1d9fdeaf995aea09a32f079e558188
Adrienne Zhang <70525565+AdrienneZhang@users.noreply.github.com>
2025-10-02 10:39:29 -0700

    Update Finding Updated Records (10299).sql

--- SQL/Finding Updated Records (10299).sql
+++ SQL/Finding Updated Records (10299).sql
@@ -1,3 +1,16 @@
+-- Problem: We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
+
+-- Attempt 1: Passed
+SELECT id,
+       first_name,
+       last_name,
+       department_id,
+       salary
+FROM
+    (SELECT *, ROW_NUMBER() OVER(PARTITION BY id ORDER BY salary DESC) AS rn
+    FROM ms_employee_salary)
+WHERE rn = 1;
+
 -- Recommended Solution
 SELECT id,
        first_name,
@@ -11,13 +24,3 @@ FROM (
 ) s
 WHERE rn = 1;
 
--- Attempt 1: Passed
-SELECT id,
-       first_name,
-       last_name,
-       department_id,
-       salary
-FROM
-    (SELECT *, ROW_NUMBER() OVER(PARTITION BY id ORDER BY salary DESC) AS rn
-    FROM ms_employee_salary)
-WHERE rn = 1;
