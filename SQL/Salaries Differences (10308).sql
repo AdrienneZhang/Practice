86e6e1fcf42db2317babd4b907893768812eb0b3
Adrienne Zhang <70525565+AdrienneZhang@users.noreply.github.com>
2025-10-02 10:38:42 -0700

    Update Salaries Differences (10308).sql

--- SQL/Salaries Differences (10308).sql
+++ SQL/Salaries Differences (10308).sql
@@ -1,3 +1,15 @@
+-- Problem: Calculates the difference between the highest salaries in the marketing and engineering departments. Output just the absolute difference in salaries.
+
+-- Attempt 1: Passed
+WITH salaries AS
+    (SELECT d.department, MAX(e.salary) salary
+        FROM db_employee as e
+        LEFT JOIN db_dept as d ON e.department_id = d.id
+        WHERE d.department IN ('engineering', 'marketing')
+        GROUP BY d.department
+    )
+SELECT MAX(salary) - MIN(salary) AS salary_difference FROM salaries;
+
 -- Recommended Solution
 SELECT ABS(MAX(CASE
                 WHEN d.department = 'engineering' THEN e.salary
@@ -8,14 +20,4 @@ FROM db_employee AS e
 LEFT JOIN db_dept AS d ON e.department_id = d.id
 WHERE d.department IN ('engineering', 'marketing')
 
--- Attempt 1: Passed
--- Initial Solution
-WITH salaries AS
-    (SELECT d.department, MAX(e.salary) salary
-        FROM db_employee as e
-        LEFT JOIN db_dept as d ON e.department_id = d.id
-        WHERE d.department IN ('engineering', 'marketing')
-        GROUP BY d.department
-    )
-SELECT MAX(salary) - MIN(salary) AS salary_difference FROM salaries;
 
