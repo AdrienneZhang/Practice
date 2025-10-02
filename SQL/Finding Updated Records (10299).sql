cd139eba47697de7536cca53234f395c51c7399c
Adrienne Zhang <70525565+AdrienneZhang@users.noreply.github.com>
2025-10-02 10:40:15 -0700

    Update Finding Updated Records (10299).sql

--- SQL/Finding Updated Records (10299).sql
+++ SQL/Finding Updated Records (10299).sql
@@ -1,4 +1,7 @@
--- Problem: We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
+/* Problem: We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. 
+Order your list by employee ID in ascending order.
+*/
 
 -- Attempt 1: Passed
 SELECT id,
